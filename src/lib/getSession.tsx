a9697e488045d462f381f7321978bdae97c71d68
Bryan Ricci <bryan.ricci@virtual-robotics.io>
2026-03-03 15:23:15 -0500

    proper ip address

--- src/lib/getSession.tsx
+++ src/lib/getSession.tsx
@@ -1,17 +1,36 @@
 import { ISession } from '@/types'
 import { notFound } from 'next/navigation'
+import { headers } from 'next/headers'
+import axios from 'axios';
 
 import 'server-only'
 
+export const createAxiosHeaders = (userAgent: string, session?: ISession) => {
+  axios.interceptors.request.use((request) => {
+    request.headers['User-Agent'] = userAgent
+    request.headers['Authorization'] = session?.token ? `Bearer ${session.token}` : ''
+    return request
+  }, function (error) {
+    return Promise.reject(error);
+  });
+}
+
 export default async function getSession(): Promise<ISession | undefined> {
-  
-  const session = await fetch(process.env.NEXT_PUBLIC_API_HOST + '/api/v1/user/session', {
+  const headersList = await headers();
+  const userAgent = headersList.get('user-agent');
+  console.log('User-Agent in getSession:', userAgent);
+  const session = await axios.get(process.env.NEXT_PUBLIC_API_HOST + '/api/v1/user/session', {
     method: 'GET',
+    headers: {
+      'User-Agent': userAgent || '',
+    }
   })
 
-  if (!session.ok) {
+  if (!session.status || session.status !== 200) {
     notFound()
   }
 
-  return session.json()
+  createAxiosHeaders(userAgent || '', session.data)
+
+  return session.data
 }
